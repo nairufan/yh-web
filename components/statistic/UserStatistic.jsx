e8379db9a9a44a8fd7a2a47cd2a01a7dfafd2556
fannairu <fannairu@gmail.com>
2016-11-04 17:39:24 +0800

    refine

--- components/statistic/UserStatistic.jsx
+++ components/statistic/UserStatistic.jsx
@@ -2,8 +2,7 @@ import React, { PropTypes, Component } from 'react';
 import Avatar from './../common/Avatar';
 import {fetch} from '../../utils/fetch';
 import moment from 'moment';
-import d3 from 'd3';
-
+import Chart from 'src/chart.js'
 const dayMillionSeconds = 24 * 60 * 60 * 1000;
 export default class Users extends Component {
 
@@ -18,16 +17,44 @@ export default class Users extends Component {
         this.getStatistics();
     }
 
+    componentDidUpdate() {
+        const {data} = this.state;
+        if (data && data.length > 0) {
+            this.renderChart(data);
+        }
+        console.log(this.state);
+    }
+
+    renderChart(data) {
+
+    }
     getStatistics() {
         const endDate = new Date().getTime();
         const startDate = endDate - 10 * dayMillionSeconds;
-        console.log(new moment(startDate).format('YYYY-MM-DD'));
-        console.log(new moment(endDate).format('YYYY-MM-DD'));
+        const data = [];
+        let tmpDate = startDate;
+        while (tmpDate <= endDate) {
+            data.push({
+                date: new moment(tmpDate).format('YYYY-MM-DD'),
+            });
+            tmpDate += dayMillionSeconds;
+        }
         let url = `/user/statistics?start=${startDate}&end=${endDate}`;
         this.setState({loading: true});
         fetch(url)
             .then((res)=> {
-                console.log(res);
+                const total = res.total;
+                const statistics = res.statistics;
+                for (let i = 0; i< data.length ;i ++) {
+                    const {date} = data[i];
+                    data[i].count = (statistics[date] && statistics[date]) || 0;
+                }
+                this.setState({
+                    loading: false,
+                    total,
+                    data,
+                });
+                console.log();
             }).catch((err)=> {
                 console.log(err);
                 this.setState({
@@ -37,10 +64,11 @@ export default class Users extends Component {
     }
 
     render() {
+        const {total} = this.state;
         return (
-            <div className='users'>
-                user statistic
-                <svg width='960' height='500'></svg>
+            <div className='user-statistic'>
+                <div className='total'>总用户数：{total || 0}</div>
+                <canvas id='myChart' width='400" height="400"></canvas>
             </div>
         )
     }
