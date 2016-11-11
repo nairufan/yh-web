bb55c9ee8d5797f39286e0a9c9bf2d2b33e809b0
fannairu <fannairu@gmail.com>
2016-11-11 17:21:48 +0800

    add html template

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,10 @@ var path = require("path");
 var webpack = require('webpack');
 var node_modules_dir = path.resolve(__dirname, 'node_modules');
 var IconFontPlugin = require("iconfont-loader/IconFontPlugin");
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 var config = {
-    devtool:false,
+    devtool: false,
     entry: {
         home: './entry/home',
         login: './entry/login',
@@ -16,7 +17,7 @@ var config = {
     output: {
         path: path.resolve(__dirname, 'static/build/'),
         filename: "[name].js",
-        publicPath: "/static/build/"
+        publicPath: "build/"
     },
 
     module: {
@@ -25,16 +26,16 @@ var config = {
                 test: /\.(js|jsx)$/,
                 loader: 'babel-loader',
                 exclude: [node_modules_dir]
-            },{
-                  test: /\.json$/,
-                  loaders: ['json'],
-                  exclude: /node_modules/
-            },{
-                  test: /\.(png|jpg)$/,
-                  loader: 'url?limit=25000'
             }, {
-                  test: /\.less/,
-                  loader: 'style-loader!css-loader!less-loader'
+                test: /\.json$/,
+                loaders: ['json'],
+                exclude: /node_modules/
+            }, {
+                test: /\.(png|jpg)$/,
+                loader: 'url?limit=25000'
+            }, {
+                test: /\.less/,
+                loader: 'style-loader!css-loader!less-loader'
             },
         ]
     },
@@ -59,6 +60,48 @@ var config = {
             "process.env": {
                 NODE_ENV: JSON.stringify("production")
             }
+        }),
+        new HtmlWebpackPlugin({
+            title: '优绘订单',
+            inject: 'body',
+            template: 'template/default.ejs',
+            filename: '../index.html',
+            chunks: ['home'],
+        }),
+        new HtmlWebpackPlugin({
+            title: '优绘订单',
+            inject: 'body',
+            template: 'template/default.ejs',
+            filename: '../login.html',
+            chunks: ['login'],
+        }),
+        new HtmlWebpackPlugin({
+            title: '优绘订单',
+            inject: 'body',
+            template: 'template/default.ejs',
+            filename: '../advice.html',
+            chunks: ['advice'],
+        }),
+        new HtmlWebpackPlugin({
+            title: '优绘订单',
+            inject: 'body',
+            template: 'template/default.ejs',
+            filename: '../detail.html',
+            chunks: ['detail'],
+        }),
+        new HtmlWebpackPlugin({
+            title: '快递查询',
+            inject: 'body',
+            template: 'template/default.ejs',
+            filename: '../express.html',
+            chunks: ['express'],
+        }),
+        new HtmlWebpackPlugin({
+            title: '快递查询',
+            inject: 'body',
+            template: 'template/default.ejs',
+            filename: '../statistic.html',
+            chunks: ['statistic'],
         }),
     ]
 };
